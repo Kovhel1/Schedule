ab27de4cfd5233fd738760a09734172ca9ee596a
Elena Kovalieva <er_kovalieva@mail.ru>
2020-07-04 21:19:48 +0300

    form login-v0, db class user-v0, связь формы и бд не прописана

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import os
-from flask import Flask, render_template, abort
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import  FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required, Email
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'very secret'
 app.config['SQLALCHEMY_DATABASE_URI']=\
     'sqlite:///' + os.path.join(basedir, 'users.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-
+class User (db.Model):
+    __tabelname__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64))
+    password = db.Column(db.String(64))
+    email = db.Column(db.String(64), unique = True )
+ 
+    
+class Login(FlaskForm):
+    username = StringField('Enter your username', validators = [Required()])
+    password = PasswordField('Enter your password', validators = [Required()])
+    email = StringField('your@mail.ru', validators = [Required(), Email()])
+    submit = SubmitField('Submit')
+    
+    
+    
+    
+    
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-
-
 @app.route('/user/<name>')
 def user(name):
     return render_template ('user.html', name=name)
+
+@app.route('/login')
+def login():
+    return render_template ('login.html')
 
 
 @app.errorhandler(404)
